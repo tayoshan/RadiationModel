d321deb5cca94c9dc4d6aba48b62b756fbb8ce30
Taylor Oshan <tayoshan@gmail.com>
2014-01-11 01:11:06 -0500

    update

--- RadiationModel.py
+++ RadiationModel.py
@@ -2,7 +2,11 @@ import numpy as np
 import matplotlib as plt
 import math
 import networkx as nx
+import igraph as ig
 from scipy.spatial import KDTree
+from igraph.datatypes import UniqueIdGenerator
+import igraph.drawing.edge as draw
+import results
 
 #To analytically predict the the commuting fluxes we consider locations i and j with population m(i) and n(j) respecitively, at ditance r(ij) from each other,
 #and we denote with s(ij) the total population in the circle of radius r(ij) centered at i (exluding the source and destination population).
@@ -10,7 +14,7 @@ from scipy.spatial import KDTree
 # T(ij) = (m(i)*n(j))/((m(i) + s(ij))*(m(i) + n(i) + s(ij)))
 
 
-#Function to calculate distance between two sites using their lat/long
+#Function to calculate distance between two sites ***This distance will eventually be the length of Least Accumulated Cost paths between origins and destinations****
 def get_dist(lat1, long1, lat2, long2):
     #degrees_to_radians = math.pi/180.0
     #phi1 = (90.0 - lat1)*degrees_to_radians
@@ -26,22 +30,26 @@ def get_dist(lat1, long1, lat2, long2):
 def get_data(file_path):
     data = np.loadtxt(open(file_path),delimiter=",",skiprows=1, dtype = str) #Load in CSV file
     data = np.array(data[:,1:], dtype = float) #Drop the sites name(easier to have no string types in the numpy array for the moment)
+    leastCP = np.loadtxt(open('C:\Users\ImAwesome\Desktop\ThesisData\LCP5\LCP_Length.txt'),delimiter="\t",skiprows=1, dtype = str)
+    dists = np.array(leastCP[:,1:], dtype = float)/100000
     x,y = data[:,2], data[:,3] #Isolate the long/lat coordinates for kdtree
     tree = KDTree(zip(x.ravel(), y.ravel())) #Create KDtree
     num_sites = np.size(data[:,1]) #Count number of sites in the data
+    sizes = data[:,4]
+    sizes[16] = 0
     pos = {} #Tuple to store coordinates for plotting results
     for site in range(num_sites):
-        pos[str(site)] =(float(data[site][3]), float(data[site][2])) #Add each sites coordinates
-    dists = np.zeros((num_sites,num_sites)) #Array to hold calculated distance for each pair of sites
-    rows,cols = np.shape(dists)
-    for r in range(rows): #calculate each distance; distance from a site to itself is always 0
-        for c in range(cols):
-            if r == c:
-                dists[r,c] = 0
-            else:
-                dists[r,c] = get_dist(float(data[r][2]),float(data[r][3]),float(data[c][2]),float(data[c][3]))
+        pos[int(site)] =(float(data[site][3]), float(data[site][2])) #Add each sites coordinates
+    #dists = np.zeros((num_sites,num_sites)) #Array to hold calculated distance for each pair of sites
+    #rows,cols = np.shape(dists)
+    #for r in range(rows): #calculate each distance; distance from a site to itself is always 0
+        #for c in range(cols):
+            #if r == c:
+                #dists[r,c] = 0
+            #else:
+                #dists[r,c] = get_dist(float(data[r][2]),float(data[r][3]),float(data[c][2]),float(data[c][3]))
 
-    return data, tree, num_sites, dists, pos
+    return data, tree, num_sites, dists, pos, sizes
 
 #Function to calculate total estimated area within distance r from an origin site
 def s(data,tree,i,j, dist):
@@ -50,14 +58,14 @@ def s(data,tree,i,j, dist):
     if len(comp_sites) > 1: #If there is more then one competing site
         for each in comp_sites:
             if each != i and each != j: #Exclude M and N from the total
-                s_total += data[each,1] #Add the estimated area (population) for each competing site
+                s_total += data[each,4] #Add the estimated area (population) for each competing site
     else:
-        s_total = data[comp_sites,1] #If there is only one then its estimated area is the total
+        s_total = data[comp_sites,4] #If there is only one then its estimated area is the total
     return s_total
 
 #Function to calculate the flow from each origin site to each destination site using radiation model
 def rad_model(data, tree, num_sites, dists, tot_ratio=1.0):
-    # tot_ratio comes from Ti from Eqn (2) of the Radiation paper
+    # tot_ratio comes from Ti from Eqn (2) of the Radiation Model paper
     # In the paper, they define Ti as equivalent to \sum_{j != i}{Tij} or more simply:
     # Ti = Mi * (Nc/N) where Nc is total # of 'commuters' and N is total population
     # For our purposes, this acts like a 'scaling' factor, and can be set to 1 by default
@@ -68,8 +76,8 @@ def rad_model(data, tree, num_sites, dists, tot_ratio=1.0):
             if i == j:
                 flows[i,j] = 0 #Flows from a site to iself is always 0 -- Not sure if this is correct yet.
             else:
-                Mi = data[i,1] #Estimated area (population) of origin site
-                Nj = data[j,1] #Estimated area (population) of destination site
+                Mi = data[i,4] #Estimated area (population) of origin site
+                Nj = data[j,4] #Estimated area (population) of destination site
                 Sij = s(data, tree, i, j, dists[i,j]) #Total estimated area of all sites r distance (or less) from M; not including M or N.
                 Ti = Mi*tot_ratio
                 Tij = (Ti*Mi*Nj)/((Mi + Sij)*(Mi + Nj + Sij))
@@ -78,40 +86,16 @@ def rad_model(data, tree, num_sites, dists, tot_ratio=1.0):
 
 
 # Preprocess data
-data, tree, num_sites, dists,pos = get_data("SiteData.csv")
-
-#Preprocess data
-data, tree, num_sites, dists,pos = get_data("C:\Users\ImAwesome\My Documents\RadiationModel\SiteDataPrjWEC.csv")
-
+data, tree, num_sites, dists, pos, sizes = get_data("SiteData.csv")
 
 # Run Model
 flows = rad_model(data, tree, num_sites, dists, tot_ratio=1.0)
 
-#Graph results
-print "Graphing proposed network"
-cutoff = np.max(flows) * 0
-G=nx.DiGraph()
-links = []
-widths = []
-justLinks = []
-rows,cols = np.shape(flows)
-out = file('flows.csv', 'w') # create output file
-for row in range(rows):
-    for col in range(cols):
-        if flows[row,col] > cutoff:
-            out.writelines("%s\t%s\t%s\n" %  (row, col, flows[row, col])) # write flows to output file
-            links.append([str(row),str(col), (flows[row,col]*1.0)])
-            widths.append(flows[row,col]*0.01)
-G.add_weighted_edges_from(links)
+data = results.initiate(flows, .01, pos, sizes)
 
+network = results.plotGraph(data)
 
+nodesOutput, netOutput = results.metrics(network, "after")
 
-nx.draw_networkx(G, pos, width = widths)
-#plt.pyplot.show()
-plt.pyplot.savefig('flows.png')
-out.close()
-
-
-nx.draw_networkx(G,pos, width = widths, alpha = .7 )
-plt.pyplot.show()
-
+nodesOutput = np.array(nodesOutput)
+np.savetxt('nodeMetsafter.csv', nodesOutput.transpose(), delimiter = ",")
